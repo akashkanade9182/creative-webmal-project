0eb1db4e9c22ed1108608389bd289ba2b2f70199
akashkanade9182 <akashkanade235@gmail.com>
2023-05-24 20:05:58 +0530

    proejct added

---
+++ frontend/src/app/Component/Homepage.jsx
@@ -0,0 +1,65 @@
+"use client"
+import "./Homepage.css"
+import React from 'react'
+import { useState,useEffect } from 'react';
+import axios from "axios"
+const getData = async (page,limit) => {
+return axios.get(`https://odd-ruby-angelfish-wear.cyclic.app/product/getproducts?page=${page}&limit=${limit}`)
+  
+};
+
+const Homepage = () => {
+     const [page, setPage] = useState(1);
+     const [data,setData]=useState([]);
+     const [totalResults,setTotalResults]=useState(0);
+
+const handlePrev=()=>{
+if(page>=1){
+  setPage(page-1)
+}
+}
+const handleNext=()=>{
+  if(page<=11){
+    setPage(page+1)
+  }
+}
+
+
+     useEffect(()=>{
+          getData(page,10).then((r)=>{
+            console.log(r.data)
+            setData(r.data.data)
+            setTotalResults(r.data.total)
+          }).catch((e)=>{
+            console.log(e);
+            alert(`${e}`)
+          })
+        },[page])
+  return (
+    <div className="homepage-box">
+      <div style={{width:"auto",fontSize:"38px",color:"black",display:"flex",justifyContent:"space-around" ,alignItems:"center"}}>
+        Products
+      </div>
+      <div className="gridbox">
+        {
+          data.length>0 && data.map((ele,index)=>(
+            <div className="card">
+              <img className="card-pic" src={ele.image} alt="" />
+              <h1 className="card-title">{ele.title}</h1>
+              </div>
+          ))
+        }
+
+      </div>
+      
+<div className="btn-box">
+  <button onClick={handlePrev}>Prev</button>
+  <div className="pagebox">{page}</div>
+  <button onClick={handleNext}>Next</button>
+
+</div>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ frontend/src/app/page.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Homepage from './Component/Homepage'
+export default function Home() {
+ 
+
+ 
+
+
+  return (
+    <main className={styles.main}>
+   <Homepage/>
+
+ 
+    </main>
+  )
+}
