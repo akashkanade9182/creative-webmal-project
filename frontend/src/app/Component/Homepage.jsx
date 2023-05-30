9c5d1edea51dbedd44e64fc8a50ba75be1ace22d
akashkanade9182 <akashkanade235@gmail.com>
2023-05-30 10:43:12 +0530

    v3

--- frontend/src/app/Component/Homepage.jsx
+++ frontend/src/app/Component/Homepage.jsx
@@ -3,6 +3,7 @@ import "./Homepage.css"
 import React from 'react'
 import { useState,useEffect } from 'react';
 import axios from "axios"
+import Loading from "./Loading";
 const getData = async (page,limit) => {
 return axios.get(`https://odd-ruby-angelfish-wear.cyclic.app/product/getproducts?page=${page}&limit=${limit}`)
   
@@ -11,6 +12,7 @@ return axios.get(`https://odd-ruby-angelfish-wear.cyclic.app/product/getproducts
 const Homepage = () => {
      const [page, setPage] = useState(1);
      const [data,setData]=useState([]);
+     const [load,setLoad]=useState(false)
      const [totalResults,setTotalResults]=useState(0);
 
 const handlePrev=()=>{
@@ -26,11 +28,15 @@ const handleNext=()=>{
 
 
      useEffect(()=>{
+      setLoad(true)
           getData(page,10).then((r)=>{
+            setLoad(false)
             console.log(r.data)
             setData(r.data.data)
             setTotalResults(r.data.total)
           }).catch((e)=>{
+            setLoad(false)
+
             console.log(e);
             alert(`${e}`)
           })
@@ -40,7 +46,8 @@ const handleNext=()=>{
       <div style={{width:"auto",fontSize:"38px",color:"black",display:"flex",justifyContent:"space-around" ,alignItems:"center"}}>
         Products
       </div>
-      <div className="gridbox">
+      {
+        load?<Loading/>:<div className="gridbox">
         {
           data.length>0 && data.map((ele,index)=>(
             <div className="card">
@@ -51,13 +58,15 @@ const handleNext=()=>{
         }
 
       </div>
-      
+
+      }  
 <div className="btn-box">
   <button onClick={handlePrev}>Prev</button>
   <div className="pagebox">{page}</div>
   <button onClick={handleNext}>Next</button>
 
 </div>
+
     </div>
   )
 }
---
+++ frontend/src/app/Component/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Loading.css"
+const Loading = () => {
+  return (
+    <div className="loadbox">
+     <div className="custom-loader"></div>
+    </div>
+  )
+}
+
+export default Loading
